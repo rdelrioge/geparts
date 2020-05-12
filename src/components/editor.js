5dd80ca84b33053ffb36525ce66510ce0f9f6717
Ricardo Del Rio <rdelrio.ge@gmail.com>
2020-05-12 01:18:52 -0500

    datatable, favicon, upload OK

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import moment, { months } from 'moment'
+import 'moment/locale/es' 
+
 import "./editor.scss";
-import { CSVToArray } from "./csvToArray";
 import { db } from "../index";
+import { CSVToArray } from "./csvToArray";
 import Tabla from "./tabla";
 
 const Edit = () => {
+
+  let d = new Date();
+  let mes = d.getMonth();
+  let year = d.getFullYear();
+  const [miPeriodo, setMiPeriodo] = useState(moment(mes, 'DD/MM/YYYY').format('MMMM') + " " + year);
+  const [filtro, setFiltro] = useState('');
+
+  const [read, setRead] = useState(false);
+  const [optionsOnSelect, setOptionsOnSelect] = useState([]);
+
   const uploadData = e => {
     e.preventDefault();
     // detectar si es un archivo CSV
@@ -21,18 +34,34 @@ const Edit = () => {
         // convertir array de arrays en objeto de objetos
         let parentO = {};
         let childO = {};
-        for (let i = 1; i < arr.length; i++) {
-          for (let j = 1; j <= arr[0].length; j++) {
+        for (let i = 1; i < arr.length - 1; i++) {
+          let periodo="";
+          for (let j = 0; j < arr[0].length; j++) {
             // si algun dato es undefined, cambiarlo para que no mande error
-            if (arr[i][j - 1] === undefined) {
-              childO[arr[0][j - 1]] = "";
+            if (arr[i][j] === undefined) {
+              childO[arr[0][j]] = "";
             } else {
-              childO[arr[0][j - 1]] = arr[i][j - 1];
+              // guardar periodo
+              if(j===3){
+                periodo= moment(arr[i][j], "DD/MM/YYYY").format('MMMM') + " " + moment(arr[i][j], "DD/MM/YYYY").year()
+              }
+              // Si es fecha... cabiarla a timestamp
+              if (j === 3 || j === 14 || j === 16 || j === 17) {
+                let creationdate = "";
+                if(arr[i][j] !==""){
+                  creationdate = moment(arr[i][j], "DD/MM/YYYY HH:mm").valueOf();
+                }
+                childO[arr[0][j]] = creationdate;
+              }else{
+                childO[arr[0][j]] = arr[i][j];
+              }
             }
           }
-          parentO[i] = { ...childO };
+          parentO[i] = { ...childO, periodo};
+          console.log(parentO[i]);
+          
           // subir objeto a base de datos con autoID
-          db.collection("refas1")
+          db.collection("refas2")
             .add(parentO[i])
             .then(docRef => console.log("Doc written with ID: ", docRef.id))
             .catch(err => console.log("Error addign doc: ", err));
@@ -44,20 +73,60 @@ const Edit = () => {
     }
   };
 
+  const mesAtras = () =>{
+    let miNewPeriodo = moment(miPeriodo, "MMMM/YYYY").subtract(1,'months').format('MMMM YYYY')
+    setMiPeriodo(miNewPeriodo);
+  }
+
+  const mesAdelante = () => {
+    let miNewPeriodo = moment(miPeriodo, "MMMM/YYYY").add(1,'months').format('MMMM YYYY')
+    setMiPeriodo(miNewPeriodo);
+  }
+
   return (
     <div>
-      <div className="input">
-        <input
-          className="inputFile"
-          type="file"
-          id="file-input"
-          onChange={async e => uploadData(e)}
-        />
-        <label className="file-input__label" htmlFor="file-input">
-          <span>Upload csv file</span>
-        </label>
+      <div className="editorHeader" >
+        <div>Ya funciona el select, falta CSS en el, y filtrar la data</div>
+        <div className="periodoClass">
+          <h2>
+            {miPeriodo}
+          </h2>
+          <div className="buttonsPeriodo">
+            <button onClick={()=>{mesAtras()}}  > {'<'} </button>
+            <button onClick={()=>{mesAdelante()}}  > {'>'} </button>
+          </div>
+        </div>
+        <div>
+          <select onFocus={() =>{ read ? setRead(false) : setRead(true)}} onChange={e => (e.target.value === '' ? null : setFiltro(e.target.value))} >
+            {
+              optionsOnSelect.length>0 ? (
+                <>
+                  <option value='' >Filtrar Checkpoint</option>
+                  {
+                    optionsOnSelect &&
+                      optionsOnSelect.map((opt, i) => (
+                        <option value={opt} key={i}> {opt} </option>
+                      )) 
+                  }
+                </>
+              ) :
+              <option value='' >Filtrar Checkpoint</option>
+            }
+          </select> 
+          <div className="input">
+            <input
+              className="inputFile"
+              type="file"
+              id="file-input"
+              onChange={async e => uploadData(e)}
+            />
+            <label className="file-input__label" htmlFor="file-input">
+              <span>Subir CSV</span>
+            </label>
+          </div>
+        </div>
       </div>
-      <Tabla />
+      <Tabla periodo={miPeriodo} filtro={filtro} read={read} datosfiltro={data => setOptionsOnSelect(data)} />
     </div>
   );
 };
--- src/components/tabla.js
+++ src/components/tabla.js
@@ -1,30 +1,80 @@
 import React, { useState, useEffect } from "react";
+import moment from "moment";
+
 import "./tabla.scss";
 import { db } from "../index";
 
-const Tabla = () => {
+const Tabla = (props) => {
+  const [refacciones, setRefacciones] = useState([]);
   const [data, setData] = useState([]);
   const [item, setItem] = useState();
   const [open, setOpen] = useState(false);
 
-  // READ FOR refacciones IN DB
-  useEffect(() => {
-    db.collection("refas1").onSnapshot(data => {
+  // READ FOR refacciones IN DB at begining
+  useEffect(()=>{
+    db.collection("refas2").orderBy("Creation_date", "desc").onSnapshot(data => {
+      console.log('read BD');
       let misRefacciones = [];
       data.forEach(refa => {
         let serv = { ...refa.data(), uid: refa.id };
         misRefacciones.push(serv);
       });
-      setData(misRefacciones);
-    });
-  }, []);
+      setRefacciones(misRefacciones);
+      let delmes = filterByProperty(
+        [...misRefacciones],
+        "periodo",
+        props.periodo
+        );
+        setData(delmes);
+      });
+  },[])
+  // Filtrar por periodo
+  useEffect(() => {
+      let delmes = filterByProperty(
+        [...refacciones],
+        "periodo",
+        props.periodo
+      );
+      setData(delmes);
+      // reset select filtrado
+      props.datosfiltro([]);
+  }, [props.periodo]);
+
+  // Leer las opciones a poner en el Select
+  useEffect(() =>{
+    let newArrfull=[];
+    for(let i=0; i<data.length; i++){
+      newArrfull.push(data[i]["Último Checkpoint"])
+    }
+    // eliminar duplicados
+    let newArr = [...new Set(newArrfull)];
+    // enviar las opciones al Select en ParentComponent
+    props.datosfiltro(newArr)
+  },[data])
+
+  // 
+  useEffect(()=>{
+    console.log(props.filtro);
+  },[props.filtro])
+
+  const filterByProperty = (array, prop, value) => {
+    var filtered = [];
+    for (var i = 0; i < array.length; i++) {
+      var obj = array[i];
+      if (obj[prop].indexOf(value) >= 0) {
+        filtered.push(obj);
+      }
+    }
+    return filtered;
+  };
 
   const toogle = () => {
     open ? setOpen(false) : setOpen(true);
   };
   return (
     <div>
-      <div className="tabla">
+      {data.length > 0 ? (
+        <>
         <ul className="tablaHeader">
           <li>LINE ID</li>
           <li>Order Number</li>
@@ -49,7 +99,9 @@ const Tabla = () => {
           <li>Último Checkpoint</li>
           <li>Comentarios</li>
           <li>Fecha de Ingreso a CC</li>
+          <li>Editar</li>
         </ul>
+      <div className="tabla">
         {data &&
           data.map((line, i) => (
             <ul
@@ -67,7 +119,7 @@ const Tabla = () => {
                   ? "even cerrado"
                   : "odd cerrado"
               }
-              onClick={() => {
+              onDoubleClick={() => {
                 setItem(i);
                 toogle();
               }}
@@ -80,7 +132,7 @@ const Tabla = () => {
               <li>{line.LINE_ID}</li>
               <li>{line["Order_ Number"]}</li>
               <li>{line["Return Reference"]}</li>
-              <li>{line["Creation_date"]}</li>
+              <li>{line["Creation_date"] === '' ? '' : moment(line["Creation_date"]).format("DD MMM YY HH:mm")}</li>
               <li>{line["Carrier"]}</li>
               <li>{line["Waybill"]}</li>
               <li>{line["Source WH"]}</li>
@@ -91,18 +143,25 @@ const Tabla = () => {
               <li>{line["FE Name"]}</li>
               <li>{line["Shipping Instructions"]}</li>
               <li>{line["SHIP_TO_Address"]}</li>
-              <li>{line["Fecha Arribo"]}</li>
+              <li>{line["Fecha Arribo"] === '' ? '' : moment(line["Fecha Arribo"]).format("DD MMM")}</li>
               <li>{line["Guia local"]}</li>
-              <li>{line["Ship Date"]}</li>
-              <li>{line["Delivery date"]}</li>
+              <li>{line["Ship Date"] === '' ? '' :  moment(line["Ship Date"]).format("DD MMM")}</li>
+              <li>{line["Delivery date"] === '' ? '' :  moment(line["Delivery date"]).format("DD MMM")}</li>
               <li>{line["Delivery Time"]}</li>
               <li>{line["Receipt"]}</li>
               <li>{line["Último Checkpoint"]}</li>
               <li>{line["Comentarios"]}</li>
               <li>{line["Fecha de Ingreso a CC"]}</li>
+              <li><button onClick={()=>{
+                console.log(line);                
+              }}  ><svg><g><path d="M3 17.25v3.75h3.75l11.06-11.06-3.75-3.75-11.06 11.06zm17.71-10.21c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></g></svg></button></li>
             </ul>
           ))}
       </div>
+      </>
+      ) : (
+        <p className="sinDatos" >No hay datos para este periodo</p>
+      )}
     </div>
   );
 };
